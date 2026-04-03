3bab5087063d2d29390233de7257c857fe256723
Konstantin S. <havendv@gmail.com>
2026-04-03 09:07:15 +0400

    feat: Updated OpenAPI spec (#99)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Tripo/Generated/Tripo.TripoClient.CreateTask.g.cs
+++ src/libs/Tripo/Generated/Tripo.TripoClient.CreateTask.g.cs
@@ -175,7 +175,6 @@ namespace Tripo
                 try
                 {
                     __response.EnsureSuccessStatusCode();
-
                     using var __content = await __response.Content.ReadAsStreamAsync(
 #if NET5_0_OR_GREATER
                         cancellationToken
