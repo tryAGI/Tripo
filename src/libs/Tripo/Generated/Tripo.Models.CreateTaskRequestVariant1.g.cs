cf9011b8f1c58d3240ec0ca17acb9b560422061d
Konstantin S. <havendv@gmail.com>
2026-05-11 01:52:29 +0400

    feat: Updated OpenAPI spec (#114)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Tripo/Generated/Tripo.Models.CreateTaskRequestVariant1.g.cs
+++ src/libs/Tripo/Generated/Tripo.Models.CreateTaskRequestVariant1.g.cs
@@ -96,5 +96,6 @@ namespace Tripo
         public CreateTaskRequestVariant1()
         {
         }
+
     }
 }
--- src/libs/Tripo/Generated/Tripo.Models.UploadFileResponseData.g.cs
+++ src/libs/Tripo/Generated/Tripo.Models.UploadFileResponseData.g.cs
@@ -40,5 +40,6 @@ namespace Tripo
         public UploadFileResponseData()
         {
         }
+
     }
 }
