4176091c55bb8b1dd4d010d15a9703cde49ac866
Konstantin S. <havendv@gmail.com>
2026-08-29 10:57:36 +0400

    feat: Updated OpenAPI spec (#145)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Tripo/Generated/Tripo.JsonSerializerContext.g.cs
+++ src/libs/Tripo/Generated/Tripo.JsonSerializerContext.g.cs
@@ -26,6 +26,7 @@ namespace Tripo
             typeof(global::Tripo.JsonConverters.UnixTimestampJsonConverter),
         })]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Tripo.JsonSerializerContextTypes))]
+    [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::System.Collections.Generic.List<object>), TypeInfoPropertyName = "SystemCollectionsGeneric_ObjectList")]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Tripo.TextToModelRequest))]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(string))]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(int))]
