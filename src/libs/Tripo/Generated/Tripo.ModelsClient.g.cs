6e4550211975bd66393ed56dd1af6ff6a00bf824
Konstantin S. <havendv@gmail.com>
2026-09-13 19:47:26 +0400

    feat: Updated OpenAPI spec (#151)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Tripo/Generated/Tripo.ModelsClient.g.cs
+++ src/libs/Tripo/Generated/Tripo.ModelsClient.g.cs
@@ -34,7 +34,7 @@ namespace Tripo
         /// <inheritdoc/>
         public global::Tripo.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Tripo.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Tripo.ModelsSourceGenerationContext.Default);
 
         /// <summary>
         ///
